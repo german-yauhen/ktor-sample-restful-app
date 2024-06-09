cb1d29957c4b103583d34862d5463f2958ad22e6
Yauheni Hermanovich <german.yauhen@gmail.com>
2024-06-09 22:17:30 +0200

    YH - Splitted route into separate ones

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,13 +1,18 @@
 package com.example.plugins
 
+import com.example.model.Priority
 import com.example.model.TaskRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.*
 
 fun Application.configureRouting() {
     routing {
+
+        staticResources("/static", "static")
 
         route("/tasks") {
             get {
@@ -20,44 +25,44 @@ fun Application.configureRouting() {
                 )
             }
 
+            get("/byName") {
+                val name = call.request.queryParameters["name"]
+                if (name.isNullOrBlank()) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+                val task = TaskRepository.withName(name)
+                if (task == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+                call.respond(
+                    ThymeleafContent(
+                        template = "all-tasks",
+                        model = mapOf("tasks" to listOf(task))
+                    )
+                )
+            }
 
+            get("/byPriority") {
+                val priorityParam = call.request.queryParameters["priority"]
+                val priority = Priority.entries.find { it.name.equals(priorityParam, ignoreCase = true) }
+                if (priority == null) {
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        "The specified $priorityParam priority is not maintained."
+                    )
+                    return@get
+                }
+                val tasks = TaskRepository.withPriority(priority)
+                call.respond(
+                    ThymeleafContent(
+                        template = "all-tasks",
+                        model = mapOf("tasks" to tasks)
+                    )
+                )
+            }
 
-//            get("/byName/{name}") {
-//                val name = call.parameters["name"]
-//                if (name.isNullOrBlank()) {
-//                    call.respond(HttpStatusCode.BadRequest)
-//                    return@get
-//                }
-//                val task = TaskRepository.withName(name)
-//                if (task == null) {
-//                    call.respond(HttpStatusCode.NotFound)
-//                    return@get
-//                }
-//                call.respond(task)
-//            }
-//
-//            get("/byPriority/{priority}") {
-//                val priorityParam = call.parameters["priority"]
-//                if (priorityParam == null) {
-//                    call.respond(HttpStatusCode.BadRequest)
-//                    return@get
-//                }
-//                val priority = Priority.entries.find { it.name.equals(priorityParam, ignoreCase = true) }
-//                if (priority == null) {
-//                    call.respond(
-//                        HttpStatusCode.BadRequest,
-//                        "The specified $priorityParam priority is not maintained."
-//                    )
-//                    return@get
-//                }
-//                val tasks = TaskRepository.withPriority(priority)
-//                if (tasks.isEmpty()) {
-//                    call.respond(HttpStatusCode.NotFound)
-//                    return@get
-//                }
-//                call.respond(tasks)
-//            }
-//
 //            post("/creation") {
 //                val task = call.receive<Task>()
 //                TaskRepository.add(task)
@@ -65,4 +70,5 @@ fun Application.configureRouting() {
 //            }
         }
     }
+
 }
